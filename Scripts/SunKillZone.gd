ff70b4b6c915747c7f9cf5b5879f7159b2fbc37d
TheGeekMax <maximesanciaume@gmail.com>
2024-06-23 14:01:54 +0200

    added working killzone + in main level

--- Scripts/SunKillZone.gd
+++ Scripts/SunKillZone.gd
@@ -1,11 +1,29 @@
 extends Area2D
 
+@onready var timer = $timer
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export_group("following")
+@export var follow_body := false
+@export var followed_body : Node2D
+@export_enum("lock x","lock y","lock both") var locking := 2
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if not follow_body:
+		return
+	
+	if locking in [0,2]:
+		position.x = followed_body.position.x
+	
+	if locking in [1,2]:
+		position.y = followed_body.position.y
+
+func _on_body_entered(body):
+	if body is Player:
+		print("skill issue")
+		Engine.time_scale = 0.5
+		timer.start()
+		
+func _on_timer_timeout():
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
